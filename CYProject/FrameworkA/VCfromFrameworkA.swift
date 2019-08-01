433f312209df2e25bba228ffdde0a42d83e94ed8
Yuan Cao <yucao@linkedin.com>
2019-08-01 17:54:56 +0800

    init

--- CYProject/FrameworkA/VCfromFrameworkA.swift
+++ CYProject/FrameworkA/VCfromFrameworkA.swift
@@ -3,39 +3,15 @@ import UIKit
 import Router
 open class VCfromFrameworkA: UIViewController {
 
-    var dependency: VCADependecyProtocol!
+    var dependency: VCADependecyProtocol = MockVCfromFrameworkADependecy()
     public var click: (()->Void)?
 
-//    open override func viewDidLoad() {
-//        self.view.backgroundColor = dependency.colorDependency.getBgColor()
-//        let button = UIButton()
-//        button.setTitle("VCA...", for: .normal)
-//        button.setTitleColor(dependency.colorDependency.getColor(), for: .normal)
-//        button.titleLabel?.font = dependency.fontDependency.getFont()
-//        button.translatesAutoresizingMaskIntoConstraints = false
-//        self.view.addSubview(button)
-//        NSLayoutConstraint.activate([
-//            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 120),
-//            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-//            button.topAnchor.constraint(equalTo: view.topAnchor, constant: 120),
-//            button.heightAnchor.constraint(equalToConstant: 40),
-//        ])
-//
-//        button.addTarget(self, action: #selector(tapButton), for: .touchUpInside)
-//    }
-//
-//    @objc func tapButton() {
-//        print("你点击了此按钮")
-//        click?()
-//    }
-
     open override func viewDidLoad() {
-        //        self.view.backgroundColor = dependency.colorDependency.getBgColor()
-        self.view.backgroundColor = UIColor.cyan
+        self.view.backgroundColor = dependency.colorDependency.getBgColor()
         let button = UIButton()
-        button.setTitle("VCA...", for: .normal)
-        button.setTitleColor(.red, for: .normal)
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 22)
+        button.setTitle("VCA", for: .normal)
+        button.setTitleColor(dependency.colorDependency.getColor(), for: .normal)
+        button.titleLabel?.font = dependency.fontDependency.getFont()
         button.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(button)
         NSLayoutConstraint.activate([
@@ -43,10 +19,34 @@ open class VCfromFrameworkA: UIViewController {
             button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             button.topAnchor.constraint(equalTo: view.topAnchor, constant: 120),
             button.heightAnchor.constraint(equalToConstant: 40),
-            ])
+        ])
 
         button.addTarget(self, action: #selector(tapButton), for: .touchUpInside)
     }
+
+//    @objc func tapButton() {
+//        print("你点击了此按钮")
+//        click?()
+//    }
+
+//    open override func viewDidLoad() {
+//        //        self.view.backgroundColor = dependency.colorDependency.getBgColor()
+//        self.view.backgroundColor = UIColor.cyan
+//        let button = UIButton()
+//        button.setTitle("VCA...", for: .normal)
+//        button.setTitleColor(.red, for: .normal)
+//        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 22)
+//        button.translatesAutoresizingMaskIntoConstraints = false
+//        self.view.addSubview(button)
+//        NSLayoutConstraint.activate([
+//            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 120),
+//            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+//            button.topAnchor.constraint(equalTo: view.topAnchor, constant: 120),
+//            button.heightAnchor.constraint(equalToConstant: 40),
+//            ])
+//
+//        button.addTarget(self, action: #selector(tapButton), for: .touchUpInside)
+//    }
 
     @objc func tapButton() {
         print("this is VCA")
