88b65575a3697137def47cd7be1e237db794a51b
Yuan Cao <yucao@linkedin.com>
2019-08-01 11:57:32 +0800

    no message

--- CYProject/VCCFortTest/VCCFortTest/AppDelegate.swift
+++ CYProject/VCCFortTest/VCCFortTest/AppDelegate.swift
@@ -7,49 +7,17 @@
 //
 
 import UIKit
-import FrameworkA
-import FrameworkB
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-//        let vc = VCABuilder.build(dependency: VCfromFrameworkADependecy()) {
-//            print("🤡👉\("hello world")👈🤡")
-//        }
-
-//        let vc = VCABuilder.build {
-//            print("🤡👉\("hello world")👈🤡")
-//
-//        }
-        let vc = VCfromFrameworkB()
+        let vc = UINavigationController(rootViewController: VCRoot())
         self.window = UIWindow()
         self.window?.rootViewController = vc
         self.window?.makeKeyAndVisible()
         return true
     }
 
-}
-
-class VCfromFrameworkADependecy: VCADependecyProtocol {
-    public init() {}
-    public var colorDependency: ColorDependencyProtocol = ColorDependency()
-    public var fontDependency: FontDependencyProtocol = FontDependency()
-}
-
-class ColorDependency: ColorDependencyProtocol {
-    func getBgColor() -> UIColor {
-        return .yellow
-    }
-
-    public func getColor() -> UIColor {
-        return .purple
-    }
-}
-
-class FontDependency: FontDependencyProtocol {
-    public func getFont() -> UIFont {
-        return UIFont.boldSystemFont(ofSize: 30)
-    }
 }
